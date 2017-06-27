0119e3b126fae13cb1bd46f1525a998a895544cb
Erika Vidal <diann852@gmail.com>
2017-06-27 13:45:49 -0500

    grid

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const state={
 
 $(_=>{
 
-    $get("http://laboratoria.cuadra.co:9339/api/v1/students/",(json)=>{
+    $getJSON("http://laboratoria.cuadra.co:9339/api/v1/students/",(json)=>{
         state.coders=json;
     })
 
