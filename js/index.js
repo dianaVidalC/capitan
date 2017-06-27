6fd3d5ecaa896353dcbe30182b6e19d977ea8217
Erika Vidal <diann852@gmail.com>
2017-06-27 13:31:38 -0500

    primer commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by LABORATORIA 0017le on 27/06/2017.
+ */
+'use strict';
+
+const state={
+    coders:null
+}
+
+$(_=>{
+
+    $get("http://laboratoria.cuadra.co:9339/api/v1/students/",(json)=>{
+        state.coders=json;
+    })
+
+    $('.asistencia').append(asistencia(state.coders));
+})
---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by LABORATORIA 0017le on 27/06/2017.
+ */
+'use strict';
+
+const asistencia=(lista)=>{
+
+    const li=$('<li>lista.name<input type="checkbox">');
+    return li;
+}
